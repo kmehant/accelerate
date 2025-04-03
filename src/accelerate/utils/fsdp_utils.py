2f0c794ca24c9b149ac1dd690ee13867907fb388
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-03 16:08:35 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -441,10 +441,9 @@ def fsdp2_load_full_state_dict(accelerator, model: torch.nn.Module, full_sd: dic
     from torch.distributed.tensor import distribute_tensor
     sharded_sd = model.state_dict()
     if accelerator.is_main_process:
-        for param_name, full_param in full_sd.items():
-            if full_param.device.type == "meta":
-                print("meta:", param_name)
         for (param_name, full_param), sharded_param in zip(full_sd.items(), sharded_sd.values()):
+            if sharded_param.device.type == "meta":
+                print("meta:", param_name)
             full_param = full_param.detach().cuda()
             mesh = sharded_param.device_mesh
             dist.broadcast(full_param, src=0, group=mesh.get_group())
