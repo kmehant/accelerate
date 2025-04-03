aa1246a48c25353e218af9a3961aa931fb7f61ad
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-03 16:13:00 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -449,12 +449,12 @@ def fsdp2_load_full_state_dict(accelerator, model: torch.nn.Module, full_sd: dic
             sharded_sd[param_name] = sharded_tensor
     else:
         for param_name, sharded_param in sharded_sd.items():
-            if sharded_param.device.type == "meta":
-                print("meta:", param_name)
             full_tensor = torch.empty(sharded_param.size(), device="cuda", dtype=sharded_param.dtype)
             mesh = sharded_param.device_mesh
             dist.broadcast(full_tensor, src=0, group=mesh.get_group())
             sharded_tensor = distribute_tensor(full_tensor, mesh, sharded_param.placements)
+            if sharded_tensor.device.type == "meta":
+                print("meta1:",param_name)
             sharded_sd[param_name] = sharded_tensor
 
     model.load_state_dict(sharded_sd)
