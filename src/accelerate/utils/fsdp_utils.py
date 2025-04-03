8fed838e0fa5f8d266d9bde4da1a2297c563ef6f
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-03 15:30:24 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/accelerate/utils/fsdp_utils.py
+++ src/accelerate/utils/fsdp_utils.py
@@ -457,9 +457,9 @@ def fsdp2_load_full_state_dict(accelerator, model: torch.nn.Module, full_sd: dic
             sharded_sd[param_name] = sharded_tensor
 
     model.load_state_dict(sharded_sd)
-    for param in model.parameters():
-        if param.device == "meta":
-            print(param.device)
+    # for param in model.parameters():
+    #     if param.device == "meta":
+    #         print(param.device)
 
 def fsdp2_switch_optimizer_parameters(optimizer: torch.optim.Optimizer, mapping: dict):
     """
